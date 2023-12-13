81addeab764392bd2ede390d0c26c0c83bddd7f5
Bamiyor <Bamiyor>
2023-12-13 13:50:56 -0800

    stacks

---
+++ README.md
@@ -0,0 +1,2 @@
+0x19. C - Stacks, Queues - LIFO, FIFO
+project done by Anyasi Bamiyor and Orji Mabel
---
+++ monty.h
@@ -0,0 +1,6 @@
+#ifndef _MONTY_H_
+#define _MONTY_H_
+
+void pchar(stack_t **head, unsigned int counter);
+
+#endif
