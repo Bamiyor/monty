f469d4f583888c43a8bcd940517e6f820f4ced78
mabelorji41 <mabelorji74@gmail.com>
2023-12-14 13:35:29 -0800

    stacks

--- pop.c
+++ pop.c
@@ -9,6 +9,8 @@
 
 void f_pop(stack_t **head, unsigned int counter)
 {
+	stack_t *h;
+
 	if (*head == NULL)
 {
 	fprintf(stderr, "L%d: can't pop an empty stack\n", counter);
@@ -17,7 +19,8 @@ void f_pop(stack_t **head, unsigned int counter)
 	free_stack(*head);
 	exit(EXIT_FAILURE);
 }
-	*head = top->next;
-	free(top);
+	h = *head;
+	*head = h->next;
+	free(h);
 }
 
